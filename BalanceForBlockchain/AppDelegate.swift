2842940d2dd991699d8f267e5ca9f22d4e724ec1
Ben Baron <ben@einsteinx2.com>
2017-06-09 13:54:57 +0200

    Initial commit

---
+++ BalanceForBlockchain/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BalanceForBlockchain
+//
+//  Created by Benjamin Baron on 6/9/17.
+//  Copyright © 2017 Balanced Software, Inc. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BalanceForBlockchain/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BalanceForBlockchain
+//
+//  Created by Benjamin Baron on 6/9/17.
+//  Copyright © 2017 Balanced Software, Inc. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
