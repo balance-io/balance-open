d45e14281767441985b0a2cf0775056902ef42e2
Ben Baron <ben@einsteinx2.com>
2017-06-16 16:21:36 +0100

    Updated Coinbase client id

--- BalanceOpen/Data Model/API Models/CoinbaseApi.swift
+++ BalanceOpen/Data Model/API Models/CoinbaseApi.swift
@@ -14,7 +14,7 @@ typealias SuccessErrorBlock = (_ success: Bool, _ error: Error?) -> Void
 fileprivate let connectionTimeout = 30.0
 //fileprivate let subServerUrl = "http://localhost:8080/"
 fileprivate let subServerUrl = "https://bal-subscription-server-beta.appspot.com/"
-fileprivate let clientId = "e47cf82db1ab3497eb06f96bcac0dde027c90c24a977c0b965416e7351b0af9f"
+fileprivate let clientId = "a6e15fbb0c3362b74360895f261fb079672c10eef79dcb72308c974408c5ce43"
 
 // Save random state for current authentication request
 fileprivate var lastState: String? = nil
