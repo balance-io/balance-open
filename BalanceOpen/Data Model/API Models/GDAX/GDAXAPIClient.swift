e02b4d67c02528dd56128ca68a61289428ff2b2b
Red Davis <me@red.to>
2017-07-26 13:01:25 +0100

    [Internal] Server

--- BalanceOpen/Data Model/API Models/GDAX/GDAXAPIClient.swift
+++ BalanceOpen/Data Model/API Models/GDAX/GDAXAPIClient.swift
@@ -11,7 +11,38 @@ import Foundation
 
 internal final class GDAXAPIClient
 {
+    // Private
+    private let server: Server
     
+    // MARK: Initialization
+    
+    internal required init(server: Server)
+    {
+        self.server = server
+    }
+}
+
+// MARK: Server
+
+internal extension GDAXAPIClient
+{
+    internal enum Server
+    {
+        case sandbox, production
+        
+        // MARK: URL
+        
+        internal func url() -> URL
+        {
+            switch self
+            {
+            case .production:
+                return URL(string: "https://api.gdax.com")!
+            case .sandbox:
+                return URL(string: "https://api-public.sandbox.gdax.com")!
+            }
+        }
+    }
 }
 
 
