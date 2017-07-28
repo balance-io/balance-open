e8797340a33b7e0e2ac3bfbab48a4468d000e285
Red Davis <me@red.to>
2017-07-28 10:19:18 +0100

    [UI] base GDAX auth view controller

---
+++ BalanceOpen/UI/Views/GDAXAuthViewController.swift
@@ -0,0 +1,18 @@
+//
+//  GDAXAuthViewController.swift
+//  BalanceOpen
+//
+//  Created by Red Davis on 28/07/2017.
+//  Copyright © 2017 Balanced Software, Inc. All rights reserved.
+//
+
+import Cocoa
+
+class GDAXAuthViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
