ec771a3d093b912729cdae0b13d5f3e5f9b06795
Red Davis <me@red.to>
2017-08-04 08:17:14 +0100

    [UI] base transfer funds view controller

---
+++ BalanceOpen/UI/TransferFundsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TransferFundsViewController.swift
+//  BalanceOpen
+//
+//  Created by Red Davis on 03/08/2017.
+//  Copyright © 2017 Balanced Software, Inc. All rights reserved.
+//
+
+import Cocoa
+
+class TransferFundsViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
