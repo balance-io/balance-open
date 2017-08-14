f7c8dfb3ddc7482ae3c034c596917f9cc4529d66
Red Davis <me@red.to>
2017-08-14 10:47:14 +0100

    [Internal] transfer request model

--- BalanceOpen/Data Model/Static & Singletons/Transfer Controller/Models/TransferRequest.swift
+++ BalanceOpen/Data Model/Static & Singletons/Transfer Controller/Models/TransferRequest.swift
@@ -7,3 +7,69 @@
 //
 
 import Foundation
+
+
+internal struct TransferRequest
+{
+    // Internal
+    internal let sourceAccount: Account
+    internal let sourceInstitution: Institution
+    internal let sourceCurrency: Currency
+    internal let recipientAddress: String
+    internal let recipientCurrency: Currency
+    internal let amount: Double
+    internal let type: RequestType
+    
+    // MARK: Initialization
+    
+    internal init(sourceAccount: Account, recipientAddress: String, recipientCurrency: Currency, amount: Double)
+    {
+        guard let sourceCurrency = Currency(rawValue: sourceAccount.currency),
+              let sourceInstitution = sourceAccount.institution else
+        {
+            throw InitializationError.unsupportedCurrency(code: sourceAccount.currency)
+        }
+        
+        self.sourceAccount = sourceAccount
+        self.sourceInstitution = sourceInstitution
+        self.sourceCurrency = sourceCurrency
+        self.recipientAddress = recipientAddress
+        self.recipientCurrency = recipientCurrency
+        self.amount = amount
+        self.type = sourceCurrency == recipientCurrency ? .direct : .exchange
+    }
+}
+
+
+// MARK: Request Type
+
+internal extension TransferRequest
+{
+    /**
+     Request type.
+     
+     - direct: No currency exchange happens (ETH to ETH, GBP to GBP).
+     - exchange: An exchange happens (ETH to BTC, BTC to GBP).
+     */
+    internal enum RequestType
+    {
+        case direct, exchange
+    }
+}
+
+
+// MARK: Initialization error
+
+internal extension TransferRequest
+{
+    /**
+     Initialization error.
+     
+     - unsupportedCurrency: Unsupported currency
+     */
+    internal enum InitializationError: Error
+    {
+        case unsupportedCurrency(code: String)
+    }
+}
+
