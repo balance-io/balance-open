6515effa25de9198fc7210786a58590049b52acf
Red Davis <me@red.to>
2017-08-15 18:49:25 +0100

    [Internal] currency text field formatter

--- BalanceOpen/UI/Utilities/CurrencyTextFieldFormatter.swift
+++ BalanceOpen/UI/Utilities/CurrencyTextFieldFormatter.swift
@@ -8,6 +8,30 @@
 
 import Cocoa
 
-class CurrencyTextFieldFormatter: NSNumberFormatter {
 
+internal final class CurrencyTextFieldFormatter: NumberFormatter
+{
+    // Private
+    private let nonDigitCharacterSet = CharacterSet.decimalDigits.inverted
+    private let decimalPointCharacterSet = CharacterSet(charactersIn: ".")
+    
+    // MARK: -
+    
+    override func isPartialStringValid(_ partialString: String, newEditingString newString: AutoreleasingUnsafeMutablePointer<NSString?>?, errorDescription error: AutoreleasingUnsafeMutablePointer<NSString?>?) -> Bool
+    {
+        guard let lastCharacter = partialString.last else
+        {
+            return true
+        }
+        
+        let currentString = String(partialString.dropLast())
+        let newCharacterString = String(lastCharacter)
+        
+        // Validations
+        let containsNonDigitCharacters = newCharacterString.rangeOfCharacter(from: self.nonDigitCharacterSet) != nil
+        let containsDecimalPoint = newCharacterString.rangeOfCharacter(from: self.decimalPointCharacterSet) != nil
+        let alreadyContainsDecimalPoint = currentString.rangeOfCharacter(from: self.decimalPointCharacterSet) != nil
+        
+        return (containsDecimalPoint && !alreadyContainsDecimalPoint) || !containsNonDigitCharacters
+    }
 }
