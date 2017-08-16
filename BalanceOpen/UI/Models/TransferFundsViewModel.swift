0820d3ff11ae364616b1d7467aadebe0eb17e587
Red Davis <me@red.to>
2017-08-16 16:07:00 +0100

    [Internal] transfer funds view model

--- BalanceOpen/UI/Models/TransferFundsViewModel.swift
+++ BalanceOpen/UI/Models/TransferFundsViewModel.swift
@@ -7,3 +7,56 @@
 //
 
 import Foundation
+
+
+internal final class TransferFundsViewModel
+{
+    // Internal
+    internal var sourceAccount: Account?
+    internal var recipientAccount: Account?
+    
+    internal var accountNames: [String] {
+        return Array(self.accounts.keys)
+    }
+    
+    internal var sourceCurrencies: [Currency] {
+        let usd = Currency.usd // TODO: This will be the selected "global" currency
+        
+        guard let sourceCurrencyValue = self.sourceAccount?.currency,
+              let sourceCurrency = Currency(rawValue: sourceCurrencyValue) else
+        {
+            return [usd]
+        }
+        
+        return [usd, sourceCurrency]
+    }
+    
+    // Private
+    private let accounts: [String : Account] = {
+        let accounts = Account.allAccounts(includeHidden: false)
+        
+        return accounts.reduce([String : Account](), { (dictionary, account) -> [String : Account] in
+            var mutableDictionary = dictionary
+            
+            // TODO: It's possible that accounts have the same display name
+            let key = "\(account.sourceInstitutionId) \(account.displayName)"
+            mutableDictionary[key] = account
+            
+            return mutableDictionary
+        })
+    }()
+    
+    // MARK: Initialization
+    
+    internal required init()
+    {
+
+    }
+    
+    // MARK: Account
+    
+    internal func account(for key: String) -> Account?
+    {
+        return self.accounts[key]
+    }
+}
