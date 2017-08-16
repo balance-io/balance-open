9db6433d1d2a257822c174c62f752a80421d973a
Red Davis <me@red.to>
2017-08-16 16:10:56 +0100

    [Refactor] build transfer quote using source amount and SS market rates

--- BalanceOpen/Data Model/Static & Singletons/Transfer Controller/Models/TransferQuote.swift
+++ BalanceOpen/Data Model/Static & Singletons/Transfer Controller/Models/TransferQuote.swift
@@ -13,20 +13,50 @@ internal struct TransferQuote
 {
     // Internal
     internal let rate: Double
+    
     internal let minerFee: Double
+    internal let minerFeeCurrency: Currency
+    
+    internal let sourceAmount: Double
+    internal let sourceCurrency: Currency
+    
     internal let recipientAmount: Double
-    internal let depositAmount: Double
+    internal let recipientCurrency: Currency
+    
+    internal let maximumAmount: Double
+    internal let minimumAmount: Double
 }
 
 // MARK: Shape Shift
 
 internal extension TransferQuote
 {
-    internal init(shapeShiftQuote: ShapeShiftAPIClient.Quote)
+    internal init(sourceAmount: Double, marketInformation: ShapeShiftAPIClient.MarketInformation) throws
     {
-        self.rate = shapeShiftQuote.rate
-        self.minerFee = shapeShiftQuote.minerFee
-        self.recipientAmount = shapeShiftQuote.recipientAmount
-        self.depositAmount = shapeShiftQuote.depositAmount
+        guard let sourceCurrency = Currency(rawValue: marketInformation.coinPair.input.symbol.uppercased()),
+              let recipientCurrency = Currency(rawValue: marketInformation.coinPair.output.symbol.uppercased()) else
+        {
+            throw InitializationError.invalidCurrency
+        }
+        
+        self.rate = marketInformation.rate
+        self.minerFee = marketInformation.minerFee
+        self.minerFeeCurrency = recipientCurrency // SS miner currency is always in the recipient currenct
+        self.sourceAmount = sourceAmount
+        self.sourceCurrency = sourceCurrency
+        self.recipientAmount = sourceAmount * marketInformation.rate
+        self.recipientCurrency = recipientCurrency
+        self.maximumAmount = marketInformation.maximumDepositLimit
+        self.minimumAmount = marketInformation.minimumDepositLimit
+    }
+}
+
+// MARK: Initialization
+
+internal extension TransferQuote
+{
+    internal enum InitializationError: Error
+    {
+        case invalidCurrency
     }
 }
