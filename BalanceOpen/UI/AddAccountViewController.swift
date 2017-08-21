92c23ce04781be387a1910f13d68902c168b3e24
Raimon Lapuente <r.lapuente@gmail.com>
2017-08-21 17:33:37 +0200

    Clean GDAX old UI

--- BalanceOpen/UI/AddAccountViewController.swift
+++ BalanceOpen/UI/AddAccountViewController.swift
@@ -425,27 +425,3 @@ class AddAccountViewController: NSViewController {
         }
     }
 }
-
-// MARK: GDAXAuthViewControllerDelegate
-
-extension AddAccountViewController: GDAXAuthViewControllerDelegate
-{
-    func didSuccessfullyLogin(with credentials: GDAXAPIClient.Credentials, in controller: GDAXAuthViewController)
-    {
-        self.dismissViewController(controller)
-        
-        // Save credentials
-        do
-        {
-            let credentialsIdentifier = "main"
-            try credentials.save(identifier: credentialsIdentifier)
-            
-            // Create institution
-            _ = Institution(sourceId: .gdax, sourceInstitutionId: "", name: "GDAX", nameBreak: nil, primaryColor: nil, secondaryColor: nil, logoData: nil, accessToken: credentialsIdentifier)
-        }
-        catch
-        {
-            // TODO: Display error
-        }
-    }
-}
