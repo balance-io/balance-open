4784970e77f3eef375e9b5711f7d00c5ee001083
Red Davis <me@red.to>
2017-08-24 10:23:05 +0100

    [Test] base transfer controller tests

---
+++ BalanceOpenTests/Tests/Transfer Controller/TransferControllerTests.swift
@@ -0,0 +1,28 @@
+//
+//  TransferControllerTests.swift
+//  BalanceOpenTests
+//
+//  Created by Red Davis on 24/08/2017.
+//  Copyright © 2017 Balanced Software, Inc. All rights reserved.
+//
+
+import XCTest
+@testable import BalanceOpen
+
+
+internal final class TransferControllerTests: XCTestCase
+{
+    // MARK: Setup
+    
+    override func setUp()
+    {
+        super.setUp()
+    }
+    
+    override func tearDown()
+    {
+        super.tearDown()
+    }
+    
+    // MARK: Initialization tests
+}
