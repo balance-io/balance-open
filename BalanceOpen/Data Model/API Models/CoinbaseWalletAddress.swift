dc32141c8f952b289476672cca631cbc7e552efe
Red Davis <me@red.to>
2017-08-24 11:41:58 +0100

    [Internal] base CB wallet address model

---
+++ BalanceOpen/Data Model/API Models/CoinbaseWalletAddress.swift
@@ -0,0 +1,9 @@
+//
+//  CoinbaseWalletAddress.swift
+//  BalanceOpen
+//
+//  Created by Red Davis on 24/08/2017.
+//  Copyright © 2017 Balanced Software, Inc. All rights reserved.
+//
+
+import Foundation
