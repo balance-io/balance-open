790ddf476a0745779ac5b00bf6bfe45dd1167ac2
Red Davis <me@red.to>
2017-09-05 14:48:41 +0100

    [UI] show accounts list as root controller

--- Balance/iOS/View Controllers/RootViewController.swift
+++ Balance/iOS/View Controllers/RootViewController.swift
@@ -6,8 +6,80 @@
 //  Copyright © 2017 Balanced Software, Inc. All rights reserved.
 //
 
-import Cocoa
+import SnapKit
+import UIKit
 
-class RootViewController: UIViewController {
 
+internal final class RootViewController: UIViewController
+{
+    // Internal
+    
+    // Private
+    private let accountsListViewController = AccountsListViewController()
+    private let rootNavigationController: UINavigationController
+    
+    // MARK: Initialization
+    
+    internal required init()
+    {
+        // Root navigation controller
+        self.rootNavigationController = UINavigationController(rootViewController: self.accountsListViewController)
+        
+        super.init(nibName: nil, bundle: nil)
+        
+        self.addChildViewController(self.rootNavigationController)
+    }
+    
+    internal override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?)
+    {
+        abort()
+    }
+    
+    internal required init?(coder aDecoder: NSCoder)
+    {
+        abort()
+    }
+    
+    deinit
+    {
+        
+    }
+    
+    // MARK: View lifecycle
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        self.setUIDefaults()
+        
+        // Root navigation controller
+        self.view.addSubview(self.rootNavigationController.view)
+
+        self.rootNavigationController.view.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool)
+    {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool)
+    {
+        super.viewDidAppear(animated)
+    }
+    
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+    }
+    
+    // MARK: UI Defaults
+    
+    private func setUIDefaults()
+    {
+        // TODO:
+    }
 }
