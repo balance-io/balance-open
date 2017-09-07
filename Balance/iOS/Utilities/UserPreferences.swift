460c244359cb583be1c9f9f7f52157a6babcc1e0
Red Davis <me@red.to>
2017-09-07 10:02:49 +0100

    [Internal] base files

---
+++ Balance/iOS/Utilities/UserPreferences.swift
@@ -0,0 +1,9 @@
+//
+//  UserPreferences.swift
+//  BalanceiOS
+//
+//  Created by Red Davis on 07/09/2017.
+//  Copyright © 2017 Balanced Software, Inc. All rights reserved.
+//
+
+import Foundation
