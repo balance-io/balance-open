398faec4e6fb1ce538b3b641fc2e3840bf1e089d
Red Davis <me@red.to>
2017-09-07 13:35:19 +0100

    [Internal] base files

---
+++ Balance/iOS/View Models/InstitutionSettingsViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  InstitutionSettingsViewModel.swift
+//  BalanceiOS
+//
+//  Created by Red Davis on 07/09/2017.
+//  Copyright © 2017 Balanced Software, Inc. All rights reserved.
+//
+
+import Foundation
+
+
+internal final class InstitutionSettingsViewModel
+{
+    
+}
