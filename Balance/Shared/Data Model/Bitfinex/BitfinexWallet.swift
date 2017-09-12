9e85bf706cf746e4a4a6218047e5dd2eae0e0189
Red Davis <me@red.to>
2017-09-12 18:54:02 +0100

    [API] bitfinex wallet mode

--- Balance/Shared/Data Model/Bitfinex/BitfinexWallet.swift
+++ Balance/Shared/Data Model/Bitfinex/BitfinexWallet.swift
@@ -7,3 +7,49 @@
 //
 
 import Foundation
+
+
+internal extension BitfinexAPIClient
+{
+    internal struct Wallet
+    {
+        // Internal
+        internal let walletType: String
+        internal let currencyCode: String
+        internal let balance: Double
+        internal let availableBalance: Double?
+        internal let unsettledInterest: Double
+        
+        // MARK: Initialization
+        
+        internal init(data: [Any]) throws
+        {
+            guard data.count == 5 else
+            {
+                throw BitfinexAPIClient.ModelError.invalidJSON(json: data)
+            }
+            
+            guard let walletType = data[0] as? String,
+                  let currencyCode = data[1] as? String,
+                  let balance = data[2] as? Double,
+                  let unsettledInterest = data[3] as? Double else
+            {
+                throw BitfinexAPIClient.ModelError.invalidJSON(json: data)
+            }
+            
+            self.walletType = walletType
+            self.balance = balance
+            self.unsettledInterest = unsettledInterest
+            self.currencyCode = currencyCode
+            
+            if let availableBalanceString = data[4] as? String
+            {
+                self.availableBalance = Double(availableBalanceString)
+            }
+            else
+            {
+                self.availableBalance = nil
+            }
+        }
+    }
+}
