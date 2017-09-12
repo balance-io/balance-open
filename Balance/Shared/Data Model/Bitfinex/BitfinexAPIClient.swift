efb5d1f7a14e902cc3d48c97df36527aad3c3b17
Red Davis <me@red.to>
2017-09-12 10:09:57 +0100

    [Internal] base files

---
+++ Balance/Shared/Data Model/Bitfinex/BitfinexAPIClient.swift
@@ -0,0 +1,9 @@
+//
+//  BitfinexAPIClient.swift
+//  Balance
+//
+//  Created by Red Davis on 12/09/2017.
+//  Copyright © 2017 Balanced Software, Inc. All rights reserved.
+//
+
+import Foundation
---
+++ Balance/Shared/Data Model/Bitfinex/BitfinexCredentials.swift
@@ -0,0 +1,9 @@
+//
+//  BitfinexCredentials.swift
+//  Balance
+//
+//  Created by Red Davis on 12/09/2017.
+//  Copyright © 2017 Balanced Software, Inc. All rights reserved.
+//
+
+import Foundation
