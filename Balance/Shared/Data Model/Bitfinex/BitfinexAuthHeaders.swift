df337c4df4aba59368feaf2197e14e52a5c876b8
Red Davis <me@red.to>
2017-09-12 18:54:27 +0100

    [API] bitfinex api auth headers

--- Balance/Shared/Data Model/Bitfinex/BitfinexAuthHeaders.swift
+++ Balance/Shared/Data Model/Bitfinex/BitfinexAuthHeaders.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+
+
+internal extension BitfinexAPIClient
+{
+    internal struct AuthHeaders
+    {
+        // Internal
+        internal let dictionary: [String : String]
+        
+        // MARK: Initialization
+        
+        internal init(credentials: Credentials, requestPath: String, body: Data?) throws
+        {
+            let nowDate = Date()
+            let signature = try credentials.generateSignature(date: nowDate, requestPath: requestPath, body: body)
+            
+            self.dictionary = [
+                "bfx-apikey" : credentials.components.key,
+                "bfx-signature" : signature,
+                "bfx-nonce" : "\(nowDate.timeIntervalSince1970)"
+            ]
+        }
+    }
+}
