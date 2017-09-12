afec35bb2bfca557749233dc038e5c7b8e00d3b0
Red Davis <me@red.to>
2017-09-12 18:54:56 +0100

    [API] bitfinex errors

--- Balance/Shared/Data Model/Bitfinex/BitfinexErrors.swift
+++ Balance/Shared/Data Model/Bitfinex/BitfinexErrors.swift
@@ -7,3 +7,46 @@
 //
 
 import Foundation
+
+
+// MARK: Model error
+
+extension BitfinexAPIClient {
+    enum ModelError: LocalizedError {
+        case invalidJSON(json: Any)
+        
+        var errorDescription: String? {
+            switch self {
+            case .invalidJSON:
+                return "There was a problem reaching the server."
+            }
+        }
+    }
+}
+
+
+// MARK: APIError
+
+extension BitfinexAPIClient {
+    enum APIError: LocalizedError {
+        case invalidJSON
+        case response(httpResponse: HTTPURLResponse, data: Data?)
+        
+        // MARK: Message
+        
+        var errorDescription: String? {
+            switch self {
+            case .invalidJSON:
+                return "Invalid JSON"
+            case .response(_, let data):
+                guard let unwrappedData = data,
+                    let json = try? JSONSerialization.jsonObject(with: unwrappedData, options: []) as? [String : Any],
+                    let message = json?["message"] as? String else {
+                        return nil
+                }
+                
+                return message
+            }
+        }
+    }
+}
