bd16f65840f9863e748f3bdc5f40e7ccba93d813
Red Davis <me@red.to>
2017-09-12 18:50:59 +0100

    [Refactor] remove unused code

--- Balance/Shared/Data Model/APIs/GDAX/GDAXErrors.swift
+++ Balance/Shared/Data Model/APIs/GDAX/GDAXErrors.swift
@@ -23,32 +23,8 @@ extension GDAXAPIClient {
     }
 }
 
-// MARK: Credentials error
 
-//extension GDAXAPIClient {
-//    enum CredentialsError: LocalizedError {
-//        case noCredentials
-//        case invalidSecret(message: String)
-//        case bodyNotValidJSON
-//        case dataNotFound(identifier: String)
-//        case missingPermissions
-//
-//        var errorDescription: String? {
-//            switch self {
-//            case .bodyNotValidJSON:
-//                return "There was a problem reaching the server."
-//            case .noCredentials:
-//                return "Invalid login credentials"
-//            case .invalidSecret(let message):
-//                return "Invalid login credentials. Make sure you have right API and Secret pair: \(message)"
-//            case .dataNotFound(let identifier):
-//                return "We couldn't reach the server: \(identifier)"
-//            case .missingPermissions:
-//                return "Your API key needs more permission, please create a new one with more permissions"
-//            }
-//        }
-//    }
-//}
+// MARK: APIError
 
 extension GDAXAPIClient {
     enum APIError: LocalizedError {
