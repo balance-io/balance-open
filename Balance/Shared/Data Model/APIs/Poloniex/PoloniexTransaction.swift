b4e9ebfb4b93d6f7c88e9bedc6b2935afd7bac19
Red Davis <me@red.to>
2017-09-25 14:43:11 +0100

    [Internal] base files

---
+++ Balance/Shared/Data Model/APIs/Poloniex/PoloniexTransaction.swift
@@ -0,0 +1,9 @@
+//
+//  PoloniexTransaction.swift
+//  Balance
+//
+//  Created by Red Davis on 25/09/2017.
+//  Copyright © 2017 Balanced Software, Inc. All rights reserved.
+//
+
+import Foundation
