d7f5dab3430044382ad84dbabcbc5167fd529f9a
Red Davis <me@red.to>
2017-10-03 16:35:31 +0100

    [Internal] base files

---
+++ Balance/iOS/View Models/TransactionsListViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  TransactionsListViewModel.swift
+//  BalanceiOS
+//
+//  Created by Red Davis on 03/10/2017.
+//  Copyright © 2017 Balanced Software, Inc. All rights reserved.
+//
+
+import Foundation
---
+++ Balance/iOS/Views/Table View/Table View Cells/TransactionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TransactionTableViewCell.swift
+//  BalanceiOS
+//
+//  Created by Red Davis on 03/10/2017.
+//  Copyright © 2017 Balanced Software, Inc. All rights reserved.
+//
+
+import UIKit
+
+class TransactionTableViewCell: TableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
