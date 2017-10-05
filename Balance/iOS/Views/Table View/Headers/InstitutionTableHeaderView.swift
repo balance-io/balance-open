b4652b814246ca9b9c3038b4cc70ae0b4ee06e67
Red Davis <me@red.to>
2017-10-05 11:21:25 +0100

    [UI] institution table header view

--- Balance/iOS/Views/Table View/Headers/InstitutionTableHeaderView.swift
+++ Balance/iOS/Views/Table View/Headers/InstitutionTableHeaderView.swift
@@ -8,14 +8,72 @@
 
 import UIKit
 
-class InstitutionTableHeaderView: UITableViewHeaderFooterView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+internal final class InstitutionTableHeaderView: UITableViewHeaderFooterView, Reusable {
+    // Static
+    internal static let height: CGFloat = 60.0
+    
+    // Internal
+    internal var institution: Institution? {
+        didSet {
+            self.reloadData()
+        }
     }
-    */
-
+    
+    // Private
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor.white
+        label.font = UIFont.systemFont(ofSize: 16.0, weight: .semibold)
+        
+        return label
+    }()
+    
+    private let totalBalanceLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor.white
+        label.font = UIFont.systemFont(ofSize: 16.0, weight: .semibold)
+        
+        return label
+    }()
+    
+    // MARK: Initialization
+    
+    internal override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        
+        // Name label
+        self.contentView.addSubview(self.nameLabel)
+        
+        self.nameLabel.snp.makeConstraints { (make) in
+            make.left.equalToSuperview().inset(15.0)
+            make.centerY.equalToSuperview()
+        }
+        
+        // Total balance label
+        self.contentView.addSubview(self.totalBalanceLabel)
+        
+        self.totalBalanceLabel.snp.makeConstraints { (make) in
+            make.right.equalToSuperview().inset(15.0)
+            make.centerY.equalToSuperview()
+        }
+    }
+    
+    internal required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
+    
+    // MARK: Data
+    
+    private func reloadData() {
+        guard let unwrappedInstitution = self.institution else {
+            return
+        }
+        
+        self.contentView.backgroundColor = unwrappedInstitution.displayColor
+        self.nameLabel.text = unwrappedInstitution.displayName
+        self.totalBalanceLabel.text = "TODO: $1,000,000"
+        
+        
+    }
 }
