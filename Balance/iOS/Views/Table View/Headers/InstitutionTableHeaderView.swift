af583e20af7424af5f8ed08a7ef6299e0b2b8af1
Red Davis <me@red.to>
2017-10-05 11:20:31 +0100

    [Internal] base files

---
+++ Balance/iOS/Views/Table View/Headers/InstitutionTableHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  InstitutionTableHeaderView.swift
+//  BalanceiOS
+//
+//  Created by Red Davis on 05/10/2017.
+//  Copyright © 2017 Balanced Software, Inc. All rights reserved.
+//
+
+import UIKit
+
+class InstitutionTableHeaderView: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Balance/iOS/Views/Table View/Table View Cells/AccountTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AccountTableViewCell.swift
+//  BalanceiOS
+//
+//  Created by Red Davis on 05/10/2017.
+//  Copyright © 2017 Balanced Software, Inc. All rights reserved.
+//
+
+import UIKit
+
+class AccountTableViewCell: TableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
