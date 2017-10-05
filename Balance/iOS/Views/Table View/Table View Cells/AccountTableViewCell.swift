9b2897db0bd9cbe8c881b636cff4a2fbc5814114
Red Davis <me@red.to>
2017-10-05 11:21:10 +0100

    [UI] add basic style to account cell

--- Balance/iOS/Views/Table View/Table View Cells/AccountTableViewCell.swift
+++ Balance/iOS/Views/Table View/Table View Cells/AccountTableViewCell.swift
@@ -8,17 +8,52 @@
 
 import UIKit
 
-class AccountTableViewCell: TableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+internal final class AccountTableViewCell: TableViewCell {
+    // Static
+    static let height: CGFloat = 50.0
+    
+    // Internal
+    internal var account: Account? {
+        didSet {
+            self.reloadData()
+        }
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    // Private
+    
+    // MARK: Initialization
+    
+    internal override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: .value1, reuseIdentifier: reuseIdentifier)
+        
+        // Text label
+        self.textLabel?.textColor = UIColor.white
+        self.textLabel?.font = UIFont.systemFont(ofSize: 14.0, weight: .regular)
+        
+        // Detail label
+        self.detailTextLabel?.textColor = UIColor.white
+        self.detailTextLabel?.font = UIFont.systemFont(ofSize: 14.0, weight: .light)
     }
-
+    
+    internal required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
+    
+    // MARK: Data
+    
+    private func reloadData() {
+        guard let unwrappedAccount = self.account else {
+            return
+        }
+        
+        self.backgroundColor = unwrappedAccount.institution?.displayColor
+        
+        // Text label
+        let currency = Currency.crypto(shortName: unwrappedAccount.currency)
+        self.textLabel?.text = amountToString(amount: unwrappedAccount.displayBalance, currency: currency)
+        
+        // Detail label
+        self.detailTextLabel?.text = "TODO: $100.00"
+    }
 }
