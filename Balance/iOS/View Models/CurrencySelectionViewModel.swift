bcc53f72554312d89d79f35b765fda0b2d09550e
Red Davis <me@red.to>
2017-10-17 15:04:03 +0100

    [Internal] protocol with default behaviour for gather all enum cases of Hashable conforming enums

---
+++ Balance/iOS/View Models/CurrencySelectionViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  CurrencySelectionViewModel.swift
+//  BalanceiOS
+//
+//  Created by Red Davis on 17/10/2017.
+//  Copyright © 2017 Balanced Software, Inc. All rights reserved.
+//
+
+import Foundation
