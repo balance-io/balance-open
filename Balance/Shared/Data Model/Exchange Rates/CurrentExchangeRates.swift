190a55b3d86b1125ef58aff9b8df7ad539296fd4
Ben Baron <ben@einsteinx2.com>
2017-10-17 13:33:01 -0500

    Moved exchange rate url to constant at top of file

--- Balance/Shared/Data Model/Exchange Rates/CurrentExchangeRates.swift
+++ Balance/Shared/Data Model/Exchange Rates/CurrentExchangeRates.swift
@@ -13,6 +13,8 @@ class CurrentExchangeRates {
         static let exchangeRatesUpdated = Notification.Name("exchangeRatesUpdated")
     }
     
+    fileprivate let exchangeRatesUrl = URL(string: "https://balance-server.appspot.com/exchangeRates")!
+    
     fileprivate let cache = SimpleCache<ExchangeRateSource, [ExchangeRate]>()
     fileprivate let persistedFileName = "currentExchangeRates.data"
     fileprivate var persistedFileUrl: URL {
@@ -77,8 +79,7 @@ class CurrentExchangeRates {
     }
     
     func updateExchangeRates() {
-        let url = URL(string: "https://balance-server.appspot.com/exchangeRates")!
-        let task = certValidatedSession.dataTask(with: url) { maybeData, maybeResponse, maybeError in
+        let task = certValidatedSession.dataTask(with: exchangeRatesUrl) { maybeData, maybeResponse, maybeError in
             // Make sure there's data
             guard let data = maybeData, maybeError == nil else {
                 log.error("Error updating exchange rates, either no data or error: \(String(describing: maybeError))")
