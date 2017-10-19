059c8bd9e962cd37d847dfe769d429db0e024b3b
Ben Baron <ben@einsteinx2.com>
2017-10-19 14:04:21 -0500

    Removed legacy sync logic to simplify it

--- Balance/Shared/Data Model/Syncing/SyncDefaults.swift
+++ Balance/Shared/Data Model/Syncing/SyncDefaults.swift
@@ -11,44 +11,13 @@ import Foundation
 class SyncDefaults {
     fileprivate struct Keys {
         static let lastSyncTimeKey = "lastSyncTime"
-        static let lastFullSyncTimeKey = "lastFullSyncTime"
         static let lastSuccessfulSyncTimeKey = "lastSuccessfulSyncTime"
-        static let lastSuccessfulFullSyncTimeKey = "lastSuccessfulFullSyncTime"
-        static let lastNewInstitutionAddedTimeKey = "lastNewInstitutionAddedTimeKey"
     }
     
     fileprivate let userDefaults = UserDefaults.standard
     
-    // Regularly sync once per hour
-    let normalSyncInterval: TimeInterval = 60.0 * 60.0
-    
-    var syncInterval: TimeInterval {
-        let intervalSinceLastNewInstitution = Date().timeIntervalSince(lastNewInstitutionAddedTime)
-        if intervalSinceLastNewInstitution <= 60 * 2 {
-            // First 2 minutes, sync every 30 seconds
-            return 30
-        } else if intervalSinceLastNewInstitution <= 60 * 5 {
-            // Next 3 minutes, sync every minute
-            return 60
-        } else if intervalSinceLastNewInstitution < 60 * 60 {
-            // For the rest of the hour, sync every 20 minutes
-            return 60 * 20
-        } else {
-            // Normal sync interval
-            return normalSyncInterval
-        }
-    }
-    
-    var fullSyncInterval: TimeInterval {
-        let intervalSinceLastNewInstitution = Date().timeIntervalSince(lastNewInstitutionAddedTime)
-        if intervalSinceLastNewInstitution <= 60 * 120 {
-            // If we've recently added an institution, always do a full sync
-            return 0
-        } else {
-            // Regularly perform a full sync once per day
-            return 60 * 60 * 24
-        }
-    }
+    // Regularly sync every 20 minutes
+    var syncInterval = 20.0 * 60.0
     
     var lastSyncTime: Date {
         get {
@@ -59,47 +28,12 @@ class SyncDefaults {
         }
     }
     
-    var lastFullSyncTime: Date {
-        get {
-            return userDefaults.object(forKey: Keys.lastFullSyncTimeKey) as? Date ?? Date.distantPast
-        }
-        set {
-            userDefaults.set(newValue, forKey: Keys.lastFullSyncTimeKey)
-        }
-    }
-    
     var lastSuccessfulSyncTime: Date {
         get {
             return userDefaults.object(forKey: Keys.lastSuccessfulSyncTimeKey) as? Date ?? Date.distantPast
         }
         set {
             userDefaults.set(newValue, forKey: Keys.lastSuccessfulSyncTimeKey)
-        }
-    }
-    
-    var lastSuccessfulFullSyncTime: Date {
-        get {
-            return userDefaults.object(forKey: Keys.lastSuccessfulFullSyncTimeKey) as? Date ?? Date.distantPast
-        }
-        set {
-            userDefaults.set(newValue, forKey: Keys.lastSuccessfulFullSyncTimeKey)
-        }
-    }
-    
-    var lastNewInstitutionAddedTime: Date {
-        get {
-            return userDefaults.object(forKey: Keys.lastNewInstitutionAddedTimeKey) as? Date ?? Date.distantPast
-        }
-        set {
-            userDefaults.set(newValue, forKey: Keys.lastNewInstitutionAddedTimeKey)
-            
-            // Schedule the next sync using the shorter syncInterval
-            async {
-                if !syncManager.syncing {
-                    NSObject.cancelPreviousPerformRequests(withTarget: syncManager, selector: #selector(SyncManager.automaticSync), object: nil)
-                    syncManager.perform(#selector(SyncManager.automaticSync), with: nil, afterDelay: self.syncInterval)
-                }
-            }
         }
     }
 }
