860afc60dd288a13e653bb57a1990ce31cf04f14
Red Davis <me@red.to>
2017-11-07 17:45:41 +0000

    [Internal] base price ticker view controller

---
+++ Balance/iOS/View Controllers/Price Ticker/PriceTickerViewController.swift
@@ -0,0 +1,25 @@
+//
+//  PriceTickerViewController.swift
+//  BalanceiOS
+//
+//  Created by Red Davis on 07/11/2017.
+//  Copyright © 2017 Balanced Software, Inc. All rights reserved.
+//
+
+import UIKit
+
+
+internal final class PriceTickerViewController: UIViewController {
+    // Private
+    
+    
+    // MARK: View lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
