f77ba11254db94f4db406b2d15018b13958ae620
Red Davis <me@red.to>
2017-11-08 10:14:49 +0000

    [Internal] base files and ticker tab bar icon

---
+++ Balance/iOS/Views/Collection View/Cells/CurrencyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CurrencyCollectionViewCell.swift
+//  BalanceiOS
+//
+//  Created by Red Davis on 08/11/2017.
+//  Copyright © 2017 Balanced Software, Inc. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyCollectionViewCell: UICollectionViewCell {
+    
+}
