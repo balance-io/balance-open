bc461735042bc95751b7b25497bf49e34b32e644
Red Davis <me@red.to>
2017-11-13 15:44:23 +0000

    [Internal] QR login credential parser

--- BalanceUnitTests/QRLoginCredentialsParserTests.swift
+++ BalanceUnitTests/QRLoginCredentialsParserTests.swift
@@ -7,29 +7,81 @@
 //
 
 import XCTest
+@testable import BalancemacOS
 
-class QRLoginCredentialsParserTests: XCTestCase {
-    
+
+internal final class QRLoginCredentialsParserTests: XCTestCase {
+    // Private
+    private let parser = QRLoginCredentialsParser()
+
+    // MARK: Setup
+
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
+
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
+
+    // MARK: Unsupported source
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    internal func testUnsupportedSource() {
+        do {
+            _ = try self.parser.parse(value: "", for: .coinbase)
+            XCTFail()
+        } catch let error as QRLoginCredentialsParser.ParseError {
+            XCTAssertEqual(error, .unsupportedSource)
+        } catch {
+            XCTFail("Invalid error")
         }
     }
     
+    // MARK: Kraken
+
+    internal func testKraken() {
+        let key = "123"
+        let secret = "456"
+        let urlString = "kraken://apikey?key=\(key)&secret=\(secret)"
+        let fields = try! self.parser.parse(value: urlString, for: .kraken)
+        
+        let keyField = fields.first { (field) -> Bool in
+            return field.type == "key"
+        }
+        
+        XCTAssertNotNil(keyField)
+        XCTAssertEqual(keyField?.value, key)
+        
+        let secretField = fields.first { (field) -> Bool in
+            return field.type == "secret"
+        }
+        
+        XCTAssertNotNil(secretField)
+        XCTAssertEqual(secretField?.value, secret)
+    }
+    
+    // MARK: Bitfinex
+    
+    internal func testBitfinex() {
+        let key = "123"
+        let secret = "456"
+        let urlString = "user:111-key:\(key)-secret:\(secret)"
+        let fields = try! self.parser.parse(value: urlString, for: .bitfinex)
+        
+        let keyField = fields.first { (field) -> Bool in
+            return field.type == "key"
+        }
+        
+        XCTAssertNotNil(keyField)
+        XCTAssertEqual(keyField?.value, key)
+        
+        let secretField = fields.first { (field) -> Bool in
+            return field.type == "secret"
+        }
+        
+        XCTAssertNotNil(secretField)
+        XCTAssertEqual(secretField?.value, secret)
+    }
+    
 }
+
