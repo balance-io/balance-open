7bba09486ede60ceb166eadfe7812f652b85d84e
Red Davis <me@red.to>
2017-11-15 10:43:29 +0000

    [Bug] correct way to set refresh control

--- Balance/iOS/View Controllers/Transactions List/TransactionsListViewController.swift
+++ Balance/iOS/View Controllers/Transactions List/TransactionsListViewController.swift
@@ -48,9 +48,9 @@ internal final class TransactionsListViewController: UIViewController {
         
         // Refresh control
         self.refreshControl.addTarget(self, action: #selector(self.refreshControlValueChanged(_:)), for: .valueChanged)
-        self.collectionView.addSubview(self.refreshControl)
         
         // Collection view
+        self.collectionView.refreshControl = self.refreshControl
         self.collectionView.backgroundColor = UIColor(red: 237.0/255.0, green: 238.0/255.0, blue: 240.0/255.0, alpha: 1.0)
         self.collectionView.dataSource = self
         self.collectionView.delegate = self
