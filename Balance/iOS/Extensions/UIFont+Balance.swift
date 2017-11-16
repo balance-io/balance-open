124c31eefbf4689b3d3bdee635e447a374a5e9a6
Red Davis <me@red.to>
2017-11-16 10:25:32 +0000

    [Internal] add custom font

--- Balance/iOS/Extensions/UIFont+Balance.swift
+++ Balance/iOS/Extensions/UIFont+Balance.swift
@@ -13,22 +13,26 @@ internal extension UIFont {
     internal struct Balance {
         static func font(ofSize size: CGFloat, weight: UIFont.Weight) -> UIFont {
             return UIFont.systemFont(ofSize: size, weight: weight)
+        }
+        
+        static func monoFont(ofSize size: CGFloat, weight: UIFont.Weight) -> UIFont {
+            for name in UIFont.familyNames {
+                print(name)
+                print(UIFont.fontNames(forFamilyName: name))
+            }
             
-            // TODO: Custom font
-//            switch weight {
-//            case .regular:
-//
-//            case .light:
-//
-//            case .bold:
-//
-//            case .semibold:
-//
-//            case .black:
-//
-//            default:
-//
-//            }
+            switch weight {
+            case .regular:
+                return UIFont(name: "SFMono-Regular", size: size)!
+            case .bold:
+                return UIFont(name: "SFMono-Bold", size: size)!
+            case .semibold:
+                return UIFont(name: "SFMono-Semibold", size: size)!
+            case .medium:
+                return UIFont(name: "SFMono-Medium", size: size)!
+            default:
+                return UIFont(name: "SFMono-Regular", size: size)!
+            }
         }
     }
 }
